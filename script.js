2304af3ae50cab149a88113a35b200f838ce339a
Caio Lemos <caioledan@gmail.com>
2024-11-26 21:40:12 -0300

    Music changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const audioElement = document.getElementById("meuAudio");
 const botaoDesmutar = document.getElementById("botaoDesmutar");
 const iconeSom = document.getElementById("iconeSom");
@@ -6,12 +5,14 @@ const iconeSom = document.getElementById("iconeSom");
 audioElement.volume = 0.1;
 
 botaoDesmutar.addEventListener("click", function() {
-    if (audioElement.muted) {
+    if (audioElement.paused || audioElement.muted) {
         audioElement.muted = false;
+        audioElement.play();
         iconeSom.src = "assets/images/com-som.png";
         iconeSom.alt = "Desativar Som";
     } else {
         audioElement.muted = true;
+        audioElement.pause();
         iconeSom.src = "assets/images/sem-som.png";
         iconeSom.alt = "Ativar Som";
     }
